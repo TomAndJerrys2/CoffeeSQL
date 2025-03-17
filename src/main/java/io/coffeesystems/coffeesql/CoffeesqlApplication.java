2c577c946df8a7dda85f156fb179039ba8e6ca0b
kylev <kylev2005@hotmail.com>
2025-03-17 21:03:37 +0000

    main

---
+++ src/main/java/io/coffeesystems/coffeesql/CoffeesqlApplication.java
@@ -0,0 +1,13 @@
+package io.coffeesystems.coffeesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeesqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeesqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/coffeesystems/coffeesql/CoffeesqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.coffeesystems.coffeesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeesqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
